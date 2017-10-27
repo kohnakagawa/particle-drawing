9d47d2bdc55a0c11f2f3fab71d9dfe2c7ed1b693
kohnakagawa <tsunekou1019@gmail.com>
2017-10-27 13:30:33 +0900

    fix segfault

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
-CXX = icpc
-#CXX = g++
+# CXX = icpc
+CXX = g++
 
-#CFLAGS = -g -O0 -Wall -std=c++11
-CFLAGS = -std=c++11 -O3 -xHOST -no-prec-div
+CFLAGS = -g -O3 -Wall -std=c++11 -ffast-math -funroll-loops -pthread
+# CFLAGS = -std=c++11 -O3 -xHOST -no-prec-div
 
 OBJECTS = sysdraw.o mousehandle.o jpegout.o main.o
 TARGET = cDraw
-LOADLIBES = -lglut -lGLU -L/usr/lib/nvidia-340-updates -lGL -ljpeg -lGLEW
+LOADLIBES = -lpthread -lglut -lGLU -lGL -ljpeg -lGLEW
 
 .SUFFIXES:
 .SUFFIXES: .cpp .o
--- main.cpp
+++ main.cpp
@@ -5,15 +5,25 @@
 #include <vector>
 #include <sstream>
 #include <map>
+#include <pthread.h>
 
 std::unique_ptr<DrawSys    > callbacks::drawsys(nullptr);
 std::unique_ptr<MouseHandle> callbacks::mousehandle(nullptr);
+
+void* simpleFunc(void*) {
+  return NULL;
+}
+
+void forcePThreadLink() {
+  pthread_t t1;
+  pthread_create(&t1, NULL, simpleFunc, NULL);
+}
 
 namespace {
   enum{
     CORRECT_OPTTAG_NUM = 8,
   };
-  
+
   void print_usege_info(){
     std::cerr << "usage:					" << std::endl;
     std::cerr << "-h (Print usage info)				" << std::endl;
@@ -23,18 +33,18 @@ namespace {
     std::cerr << "-b (Begin time)				" << std::endl;
     std::exit(1);
   }
-  
-  std::vector<std::string> 
+
+  std::vector<std::string>
   get_argvs(const int argc, char* argvs_[]){
     std::vector<std::string> argvs;
     for(int i=1; i<argc; i++) argvs.push_back(argvs_[i]);
     return argvs;
   }
-  
+
   bool input_is_correct(const std::vector<std::string>& argvs){
-    for(auto it = argvs.cbegin(); it != argvs.cend(); ++it) 
+    for(auto it = argvs.cbegin(); it != argvs.cend(); ++it)
       if(*it == "-h") return false;
-    
+
     if(argvs.size() != CORRECT_OPTTAG_NUM) return false;
     return true;
   }
@@ -49,24 +59,24 @@ namespace {
     if(tag == "-b") std::cerr << "-b begin time			" << std::endl;
     exit(1);
   }
-  
+
   void get_options(std::map<std::string, std::string>& tag_opts,
 		   const std::vector<std::string>& argvs)
   {
-    for(size_t i=0; i<argvs.size(); i += 2) 
+    for(size_t i=0; i<argvs.size(); i += 2)
       tag_opts[ argvs[i] ] = argvs[i + 1];
-    
+
     const auto end_it = tag_opts.cend();
     const bool tag_is_correct = (tag_opts.find("-i") != end_it) && (tag_opts.find("-t") != end_it) && (tag_opts.find("-o") != end_it) && (tag_opts.find("-b") != end_it);
     if(tag_is_correct){
       const bool flag_t_correct = (tag_opts["-t"] == "slide" ) || (tag_opts["-t"] == "anime");
       const bool flag_o_correct = (tag_opts["-o"] == "0"     ) || (tag_opts["-o"] == "1"    );
       const bool flag_b_correct = ( atoi(tag_opts["-b"].c_str() ) >= 0);
-      if(!flag_t_correct) dump_err_unknown_opt("-t", tag_opts["-t"]);      
+      if(!flag_t_correct) dump_err_unknown_opt("-t", tag_opts["-t"]);
       if(!flag_o_correct) dump_err_unknown_opt("-o", tag_opts["-o"]);
       if(!flag_b_correct) dump_err_unknown_opt("-b", tag_opts["-b"]);
     }else{
-      print_usege_info();      
+      print_usege_info();
     }
   }
 }
@@ -83,13 +93,13 @@ int main(int argc, char* argv[]){
 
   if(tag_opts["-t"] == "anime")
     callbacks::drawsys.reset(new AnimeDraw (cur_dir, crit_out, beg_time) );
-  else if(tag_opts["-t"] == "slide") 
+  else if(tag_opts["-t"] == "slide")
     callbacks::drawsys.reset(new SlideDraw (cur_dir, crit_out, beg_time) );
   else
     std::exit(1);
 
   callbacks::drawsys->SetParams();
-  
+
   callbacks::mousehandle.reset(new MouseHandle (0.0, 0.0, 0.0, 6.0, 17.0));
   callbacks::drawsys->GetMouseInfo(callbacks::mousehandle->RetFovy(), 
 				   callbacks::mousehandle->RetPersCent(), 
@@ -97,7 +107,7 @@ int main(int argc, char* argv[]){
 				   callbacks::mousehandle->RetEbaseZ());
 
   callbacks::drawsys->FileOpen();
-  
+
   const GLfloat water_c[]	= {0.000, 0.749, 1.000};
   const GLfloat hyphil_c[]	= {1.000, 0.188, 0.188};
   const GLfloat hyphob_c[]	= {1.000, 1.000, 0.000};
@@ -110,14 +120,14 @@ int main(int argc, char* argv[]){
   callbacks::drawsys->SetColor(catal_c);
   callbacks::drawsys->SetColor(reacted_c);
   callbacks::drawsys->SetColor(reacted_h);
-  
-  const GLfloat light0pos[] = {0.0, 0.0, -1.0, 1.0};
+
+  const GLfloat light0pos[] = {10.0, 10.0, -1.0, 1.0};
   callbacks::drawsys->SetLightPos(light0pos);
 
   callbacks::drawsys->InitCube();
   callbacks::drawsys->InitWindow(argc, argv, cur_dir.c_str() );
   callbacks::drawsys->InitGlew();
-  
+
   callbacks::drawsys->SetCallBackFunc();
   callbacks::drawsys->InitColor();
 
